24d6b391f3a69fce88d75193ffe0e6e3aa0fd6bd
Samuel Ytterbrink <neppord@gmail.com>
2020-04-10 16:20:55 +0200

    Add housing model.

--- src/Main.elm
+++ src/Main.elm
@@ -100,24 +100,67 @@ houses =
                 <|
                     text title
     in
-    column
-        [ centerX, centerY, padding 10, spacing 10 ]
-        [ row [ centerX, centerY, spacing 10 ]
-            [ house
-            , empty
-            , empty
-            ]
-        , row [ centerX, centerY, spacing 10 ]
-            [ house
-            , empty
-            , house
-            ]
-        , row [ centerX, centerY, spacing 10 ]
-            [ empty
-            , house
-            , empty
-            ]
+    row
+        [ centerX
+        , centerY
+        , padding 10
+        , spacing 10
         ]
+    <|
+        List.map
+            (\c ->
+                column
+                    [ centerX
+                    , centerY
+                    , spacing 10
+                    ]
+                <|
+                    List.map
+                        (\n ->
+                            if n > 0 then
+                                house
+
+                            else
+                                empty
+                        )
+                        c
+            )
+        <|
+            toMatrix housingModel
+
+
+type alias SmallMap a =
+    { p00 : a
+    , p10 : a
+    , p20 : a
+    , p01 : a
+    , p11 : a
+    , p21 : a
+    , p02 : a
+    , p12 : a
+    , p22 : a
+    }
+
+
+housingModel : SmallMap Int
+housingModel =
+    { p00 = 0
+    , p10 = 1
+    , p20 = 0
+    , p01 = 2
+    , p11 = 3
+    , p21 = 0
+    , p02 = 4
+    , p12 = 0
+    , p22 = 0
+    }
+
+
+toMatrix m =
+    [ [ m.p00, m.p01, m.p02 ]
+    , [ m.p10, m.p11, m.p12 ]
+    , [ m.p20, m.p21, m.p22 ]
+    ]
 
 
 main =
