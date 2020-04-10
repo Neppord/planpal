4b87fb83f6b3696d278b6949e0cf5a2f19f4efda
Samuel Ytterbrink <neppord@gmail.com>
2020-04-10 18:30:52 +0200

    Extract view function from main

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,15 @@ module Main exposing (..)
 
 import Element exposing (..)
 import Landscape exposing (landscape)
+import Model
 import Timeline exposing (timeline)
 
 
 main =
+    view Model.init
+
+
+view model =
     layout [ height fill ] <|
         row [ height fill, width fill ]
             [ timeline, landscape ]
