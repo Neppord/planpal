42949534a8061a5274ed9062fd98cb83079c1440
Samuel Ytterbrink <neppord@gmail.com>
2020-04-10 16:24:29 +0200

    Extract model from main

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
+import Model exposing (housingModel, toMatrix)
 
 
 eventAttr color =
@@ -127,40 +128,6 @@ houses =
             )
         <|
             toMatrix housingModel
-
-
-type alias SmallMap a =
-    { p00 : a
-    , p10 : a
-    , p20 : a
-    , p01 : a
-    , p11 : a
-    , p21 : a
-    , p02 : a
-    , p12 : a
-    , p22 : a
-    }
-
-
-housingModel : SmallMap Int
-housingModel =
-    { p00 = 0
-    , p10 = 1
-    , p20 = 0
-    , p01 = 2
-    , p11 = 3
-    , p21 = 0
-    , p02 = 4
-    , p12 = 0
-    , p22 = 0
-    }
-
-
-toMatrix m =
-    [ [ m.p00, m.p01, m.p02 ]
-    , [ m.p10, m.p11, m.p12 ]
-    , [ m.p20, m.p21, m.p22 ]
-    ]
 
 
 main =
