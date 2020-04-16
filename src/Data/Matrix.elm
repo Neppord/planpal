b806c1d5a3f9bb67bd2805f81e53e194a911a177
Samuel Ytterbrink <neppord@gmail.com>
2020-04-16 12:52:15 +0200

    Adda indexedMap for matrix

--- src/Data/Matrix.elm
+++ src/Data/Matrix.elm
@@ -10,6 +10,15 @@ map =
     List.map << List.map
 
 
+indexedMap : (Int -> Int -> a -> b) -> Matrix a -> Matrix b
+indexedMap f =
+    let
+        callback x column =
+            List.indexedMap (\y tile -> f x y tile) column
+    in
+    List.indexedMap callback
+
+
 toList : Matrix a -> List a
 toList =
     List.concat
